01d4fe64d46255962761145c62c70e1f9d52d7d9
andresb39 <andresb39@gmail.com>
2023-02-10 11:30:49 -0500

    FIX: Terraform plan remove -var

--- sso_permissions_set/main.tf
+++ sso_permissions_set/main.tf
@@ -14,7 +14,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "sso_policy_attachment" {
 }
 
 resource "aws_ssoadmin_permission_set_inline_policy" "sso_inline_policy" {
-  count              = var.inline_policy > 0 ? 1 : 0
+  count              = length(var.inline_policy) > 0 ? 1 : 0
   inline_policy      = var.inline_policy
   instance_arn       = aws_ssoadmin_permission_set.sso_permission_set.instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.sso_permission_set.arn
---
+++ test_env.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+export TF_VAR_group_name="My_Team"
+export TF_VAR_description="This is my team description"
+export TF_VAR_google_group_name="My_Team_google"
+export TF_VAR_sso_permission_set_arn="arn:aws:sso:::permissionSet/ssoins-7223ad97cd5b6c9c/ps-29b4323d8424c244"
+export TF_VAR_sso_permission_set_instance_arn="arn:aws:sso:::permissionSet/ssoins-7223ad97cd5b6c9c/ps-29b4323d8424c244"
+export TF_VAR_target_id=["1111111111"]
